1b3f662b4ef9551403d04c5932dc79ae9747bb3d
hi098123 <hi235750@gmail.com>
2025-02-23 01:10:44 +0900

    v5.0.0

---
+++ src/version.h
@@ -0,0 +1,7 @@
+#pragma once
+#define UPX_VERSION_HEX      0x050000 /* 05.00.00 */
+#define UPX_VERSION_STRING   "5.0.0"
+#define UPX_VERSION_STRING4  "5.00"
+#define UPX_VERSION_DATE     "Feb 20th 2025"
+#define UPX_VERSION_DATE_ISO "2025-02-20"
+#define UPX_VERSION_YEAR     "2025"
